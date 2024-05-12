e40eaebfbe920ae8fdfa2e28ed8c833dab80864b
rustieepo0 <adityaasilwal@gmail.com>
2024-05-12 12:01:17 +0530

    Added NewCont4ext and NewContract.

--- internal/contracts/in_Contracts.go
+++ internal/contracts/in_Contracts.go
@@ -10,3 +10,21 @@ type Context struct {
 	process string
 	signer  *goar.ItemSigner
 }
+
+func NewContext(ao *aogo.AO, process string, signer *goar.ItemSigner) *Context {
+	return &Context{
+		ao:      ao,
+		process: process,
+		signer:  signer,
+	}
+}
+
+type Contract struct {
+	*Context
+}
+
+func NewContract(ao *aogo.AO, process string, signer *goar.ItemSigner) *Contract {
+	return &Contract{
+		Context: NewContext(ao, process, signer),
+	}
+}
