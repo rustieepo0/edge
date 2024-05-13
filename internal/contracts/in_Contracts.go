59d0907b0072a250821883f3af351489484f3648
rustieepo0 <adityaasilwal@gmail.com>
2024-05-13 23:10:46 +0530

    Added more  functions

--- internal/contracts/in_Contracts.go
+++ internal/contracts/in_Contracts.go
@@ -58,3 +58,32 @@ func (c *Contract) GetUpload(dataItemId string) (*GetUploadResponse, error) {
 
 	return &response, err
 }
+
+func (c *Contract) Notify(dataItemId string, transactionId string) error {
+	_, err := c.ao.SendMessage(c.process, "", []types.Tag{{Name: "Action", Value: "Notify"}, {Name: "DataItemId", Value: dataItemId}, {Name: "TransactionId", Value: transactionId}}, "", c.signer)
+	return err
+}
+
+func (c *Contract) Release(dataItemId string) error {
+	_, err := c.ao.SendMessage(c.process, "release", []types.Tag{{Name: "Action", Value: "Release"}, {Name: "DataItemId", Value: dataItemId}}, "", c.signer)
+	return err
+}
+
+func (c *Contract) Stake(url string) error {
+	_, err := c.ao.SendMessage(c.process, "", []types.Tag{{Name: "Action", Value: "Stake"}, {Name: "Url", Value: url}}, "", c.signer)
+	return err
+}
+
+func (c *Contract) GetStaker() (string, error) {
+	mId, err := c.ao.SendMessage(c.process, "", []types.Tag{{Name: "Action", Value: "Staked"}}, "", c.signer)
+	if err != nil {
+		return "", err
+	}
+
+	result, err := c.ao.ReadResult(c.process, mId)
+	if err != nil {
+		return "", err
+	}
+
+	return result.Messages[0]["Data"].(string), nil
+}
