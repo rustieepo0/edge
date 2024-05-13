372f4de51073f015e08d1c0db7d1969e1f9d5d95
rustieepo0 <adityaasilwal@gmail.com>
2024-05-13 21:52:59 +0530

    GetUpload and GetUploadResponse

--- internal/contracts/in_Contracts.go
+++ internal/contracts/in_Contracts.go
@@ -1,7 +1,10 @@
 package contracts
 
 import (
+	"encoding/json"
+
 	"github.com/everFinance/goar"
+	"github.com/everFinance/goar/types"
 	"github.com/liteseed/aogo"
 )
 
@@ -27,4 +30,31 @@ func NewContract(ao *aogo.AO, process string, signer *goar.ItemSigner) *Contract
 	return &Contract{
 		Context: NewContext(ao, process, signer),
 	}
+}
+
+type GetUploadResponse struct {
+	Status   string `json:"status"`
+	Quantity string `json:"quantity"`
+	Block    string `json:"block"`
+	Bundler  string `json:"bundler"`
+}
+
+func (c *Contract) GetUpload(dataItemId string) (*GetUploadResponse, error) {
+	mId, err := c.ao.SendMessage(c.process, "", []types.Tag{{Name: "Action", Value: "Upload"}, {Name: "DataItemId", Value: dataItemId}}, "", c.signer)
+	if err != nil {
+		return nil, err
+	}
+
+	result, err := c.ao.ReadResult(c.process, mId)
+	if err != nil {
+		return nil, err
+	}
+
+	var response GetUploadResponse
+	err = json.Unmarshal([]byte(result.Messages[0]["Data"].(string)), &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, err
 }
